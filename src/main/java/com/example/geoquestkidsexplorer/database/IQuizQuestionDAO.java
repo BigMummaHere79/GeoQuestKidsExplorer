ed6557a70583694a9f77a3e2231a3a8dbddb5c45
saitoriahanipale <thanipale12@gmail.com>
2025-10-18 12:27:26 +1000

    JavaDoc comments for the Database Package

--- src/main/java/com/example/geoquestkidsexplorer/database/IQuizQuestionDAO.java
+++ src/main/java/com/example/geoquestkidsexplorer/database/IQuizQuestionDAO.java
@@ -2,6 +2,11 @@ package com.example.geoquestkidsexplorer.database;
 import com.example.geoquestkidsexplorer.models.PracticeQuizQuestions;
 import com.example.geoquestkidsexplorer.models.TestQuizQuestions;
 
+/**
+ * This interface helps with creating a Mock for unit testing
+ * Calling the main methods for the logic of Practise and Test Quiz
+**/
+
 // Created for interface used in RealQuizDataTest
 
 public interface IQuizQuestionDAO {
--- src/main/java/com/example/geoquestkidsexplorer/database/RealQuizDataSource.java
+++ src/main/java/com/example/geoquestkidsexplorer/database/RealQuizDataSource.java
@@ -2,6 +2,13 @@ package com.example.geoquestkidsexplorer.database;
 
 import com.example.geoquestkidsexplorer.database.DatabaseManager.CountryQuestion;
 
+/**
+ *
+ Instance based way to fetch quiz data instead of calling
+ static DatabaseManager methods directly.
+ Letting the controllers depend on QuizDataSource interface so we can plug mock for tests
+**/
+
 // Use Implements -> Derived from the Modules week 9 I believe for more info
 public final class RealQuizDataSource implements QuizDataSource{
 
