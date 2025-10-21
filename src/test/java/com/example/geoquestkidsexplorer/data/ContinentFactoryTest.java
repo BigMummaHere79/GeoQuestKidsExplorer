b3f2b4e4ce54763ad21327d0a3faffb225054c18
saitoriahanipale <thanipale12@gmail.com>
2025-10-22 00:25:08 +1000

    added ContinentFactory unit test

--- src/test/java/com/example/geoquestkidsexplorer/data/ContinentFactoryTest.java
+++ src/test/java/com/example/geoquestkidsexplorer/data/ContinentFactoryTest.java
@@ -1,16 +1,25 @@
 package com.example.geoquestkidsexplorer.data;
 
+import com.example.geoquestkidsexplorer.models.Continents;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ContinentFactoryTest {
 
     @Test
-    void getContinent() {
+    void testGetAllContinent() {
+        List<String> continents = ContinentFactory.getAllContinentNames();
+        assertNotNull(continents,"Name list should not be null");
+
     }
 
     @Test
-    void getAllContinentNames() {
+    void testGetContinentNames() {
+        assertNotNull(ContinentFactory.getContinent("Oceania"));
     }
 }
